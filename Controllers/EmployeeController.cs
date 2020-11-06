ba9e6be1b2d322685501e584c83970af991a5ec3
Mr Langa <user@Marks-MacBook-Air.local>
2020-11-06 21:10:28 +0200

    Update Employee Model

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,10 +1,69 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Test_MVC.Controllers
 {
-    public class EmployeeController
+    public class EmployeeController : Controller
     {
-        public EmployeeController()
+        
+        public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            return View();
         }
     }
 }
--- Models/Employee.cs
+++ Models/Employee.cs
@@ -4,6 +4,16 @@ namespace Test_MVC.Models
     public class Employee
     {
         public int Id { get; set; }
+        public int Count { get; set; }
         public int Age { get; set; }
+        public string Married { get; set; }
+        public string Over18 { get; set; }
+        public string Gender { get; set; }
+        public int Distance { get; set; }
+        public int WorkingHours { get; set; }
+        public string Attrition { get; set; }
+        public int Performancerating { get; set; }
+        public int CompaniesWorked { get; set; }
+
     }
 }
